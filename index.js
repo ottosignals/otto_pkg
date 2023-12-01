ff006dd7c83b9bd7b97542298c6faf23cc08d1e9
Otto Hrubý <otto@signals.cz>
2023-12-01 14:30:04 +0000

    a

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const file_one = require("./includes/file_one");
+const file_two = require("./includes/file_two");
 
 module.exports = (params) => {
 
@@ -10,6 +11,7 @@ module.exports = (params) => {
     // Publish and return datasets.
     let result = {
       file_one: file_one(params),
+      file_two: file_two(params),
     };
 
     return result;
